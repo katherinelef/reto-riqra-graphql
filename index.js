5e61fd29240bdf44389927ef800621ed563aa82e
katherinelef <katherine.lef@hotmail.com>
2018-06-17 03:11:40 -0500

    servidor en GraphQL con Node.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+import typeDefs from "./schemas";
+import resolvers from "./resolvers";
+
+
+const myGraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+const PORT = 3000;
+
+const app = express();
+
+// bodyParser is needed just for POST.
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+
+app.listen(PORT);
